a24c271408a54d66945a05a407b81d755956d4ca
Dante1404 <tandatngotran@gmail.com>
2023-11-04 02:16:31 +0700

    Book-finish

--- DataAccess/PRN_Group03Context.cs
+++ DataAccess/PRN_Group03Context.cs
@@ -29,7 +29,7 @@ namespace DataAccess
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=123456;database=PRN_Group03");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-TT4LHQA\\SQLEXPRESS;uid=sa;pwd=12345;database=PRN_Group03");
             }
         }
 
